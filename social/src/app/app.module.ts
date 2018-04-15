33700cef4d0c87d61144ebd9a4309126f0ac410e
merit.gabra <merit.gabra@softxpert.com>
2018-04-15 16:18:17 +0200

    social

--- social/src/app/app.module.ts
+++ social/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+// import { HomeComponent } from './home/index';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_directives/index';
 
 
 @NgModule({
   declarations: [
     AppComponent
+
+        AlertComponent,
+        //HomeComponent,
+        // LoginComponent,
+        // RegisterComponent
   ],
   imports: [
     BrowserModule
