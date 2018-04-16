f37e6eb611e67b7ef7f4c850236ea5d541a58d39
merit.gabra <merit.gabra@softxpert.com>
2018-04-16 17:51:04 +0200

    social

--- social/src/app/app.module.ts
+++ social/src/app/app.module.ts
@@ -1,25 +1,56 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+
+import { routing }        from './app.routing';
 
 import { AlertService, AuthenticationService, UserService } from './_services/index';
-// import { HomeComponent } from './home/index';
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+
 import { AppComponent } from './app.component';
+
 import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+
+import { JwtInterceptor } from './_helpers/index';
+import { fakeBackendProvider } from './_helpers/index';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
 
-        AlertComponent,
-        //HomeComponent,
-        // LoginComponent,
-        // RegisterComponent
+        AlertComponent
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
   ],
   imports: [
-    BrowserModule
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        routing
   ],
-  providers: [],
+  providers: [   
+       
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtInterceptor,
+            multi: true
+        },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+        ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
